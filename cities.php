9f27dbd5ed3390bf3d34e5c497c48b93f0e43f3c
Doug C <d.chung1245@gmail.com>
2019-12-06 15:28:11 -0700

    Added the include pages and login pages

---
+++ cities.php
@@ -0,0 +1,15 @@
+<!--this is the header for the page-->
+<?php
+    require "header.php";
+?>
+
+    <main>
+        <div class="wrapper-main">
+<!--Insert your code for this page inside the mains-->
+        </div>
+    </main>
+
+<!--this is the header for the page-->
+<?php
+    require "footer.php";
+?>
