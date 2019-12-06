2f3ab5a26e88f699725f97a641c5a4baddcec52d
DunkinChung <55110481+DunkinChung@users.noreply.github.com>
2019-12-06 16:28:34 -0700

    Update single-photo.php

--- single-photo.php
+++ single-photo.php
@@ -8,7 +8,11 @@ if($conn->connect_error){
     exit('Error connecting to the database');
 }
 
-$id = '';
+$id ='';
+
+if(isset($_GET['ImageID'])){
+    $id = $_GET['ImageID'];
+}
 
 $sql ="SELECT * FROM imagedetails WHERE ImageID=?";
 $stmt = mysqli_stmt_init($conn);
@@ -20,7 +24,6 @@ if(!mysqli_stmt_prepare($stmt, $sql)){
 else{
     if($id != null){
         mysqli_stmt_prepare($stmt, $sql);
-        //Finds "s"<string> and replaces it with variable $id
         mysqli_stmt_bind_param($stmt, "s", $id);
     }
     else{
@@ -30,9 +33,31 @@ else{
     mysqli_stmt_execute($stmt);
     $image = mysqli_stmt_get_result($stmt);
 
-    echo $image["Path"];
+    
 }
 
+$userFind = "SELECT * FROM users WHERE UserID=?";
+
+$stmt = mysqli_stmt_init($conn);
+
+if(!mysqli_stmt_prepare($stmt, $userFind)){
+    header("Location: ../index.php?error=sqlerror");
+    exit();
+}
+else{
+    if($id != null){
+        mysqli_stmt_prepare($stmt, $userFind);
+        //Finds "s"<string> and replaces it with variable $id
+        mysqli_stmt_bind_param($stmt, "i", $image['UserID']);
+    }
+    else{
+        header("Location: //index.php?error=invalidImage");
+    }
+
+    $user = mysqli_stmt_get_result($stmt);
+
+    echo $user;
+}
 
 require "header.php";
 ?>
