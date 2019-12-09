418150c4650e62bf7aa13bc87e7aeb55ab23c280
DRC-7 <d.chung1245@gmail.com>
2019-12-08 23:55:20 -0700

    Add query to get AsciiName passed in from queryString

--- single-city.php
+++ single-city.php
@@ -1,7 +1,34 @@
 <?php
 require 'includes/dbh.inc.php';
 
+if ($conn->connect_error) {
+    exit("Error connecting to the database");
+}
 
+//----- City Details Fetch -----------------------------------------------------------------------------
+if (isset($_GET['AsciiName'])) {
+    $selectedCity = $_GET['AsciiName'];
+
+    $sql = "SELECT * FROM cities WHERE AsciiName=?";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: ../index.php?error=Ascii-Name-Error");
+        exit();
+    } else {
+        if ($selectedCity != null) {
+            mysqli_stmt_prepare($stmt, $sql);
+            mysqli_stmt_bind_param($stmt, "s", $selectedCity);
+        } else {
+            header("Location: //index.php?error=passed-city-error");
+        }
+
+        mysqli_stmt_execute($stmt);
+        $city = mysqli_stmt_get_result($stmt);
+        $cityRow = mysqli_fetch_assoc($city);
+    }
+}
+mysqli_stmt_close($stmt);
 
 ?>
 
@@ -35,21 +62,29 @@ require 'includes/dbh.inc.php';
         </div>
 
         <div class='details-container'>
-            <h1><?php ?></h1>
+            <h1><?php echo $cityRow['TimeZone']; ?></h1>
             <div id='city-details'>
                 <!-- All of the data for the selected country -->
                 <?php ?>
             </div>
+
+            <div class='city-map-container'>
+                <h1>City Map</h1>
+                <div id='city-map'>
+                    <!-- Map of the selected city -->
+                </div>
+            </div>
+
         </div>
 
-        <div class='city-map-container'>
-            <h1>City Map</h1>
-            <div id='city-map'>
-                <!-- Map of the selected city -->
+        <div class='country-photo-container'>
+            <h1>City Photos</h1>
+            <div id='city-photos'>
+                <!-- All of the images for the selected city -->
+                <?php ?>
             </div>
         </div>
 
-    </div>
 
 </body>
 
