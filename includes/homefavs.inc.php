51fa6637e77b99dac2e331cd4bc6dd61bd863d99
DunkinChung <55110481+DunkinChung@users.noreply.github.com>
2019-12-09 14:36:22 -0700

    Update homefavs.inc.php

--- includes/homefavs.inc.php
+++ includes/homefavs.inc.php
@@ -1,10 +1,11 @@
 <div>
-<p>Favourites</p>
-<?php
-    if(isset($_SESSION['favPhotos'])){
-        foreach($_SESSION['favPhotos'] as $i){
-            echo '<img src="https://storage.googleapis.com/riley_comp3512_ass1_images/case-travel-master/images/large1024/$i">';
+    <p>Favourites</p>
+    <?php
+    if (isset($_SESSION['favPhotos'])) {
+        foreach ($_SESSION['favPhotos'] as $i) {
+            $img = '<img src="https://storage.googleapis.com/riley_comp3512_ass1_images/case-travel-master/images/large1024/"' . $i . ">";
+            echo $img;
         }
     }
-?>
+    ?>
 </div>
