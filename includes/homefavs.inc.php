ac0e72bfab9fd3a320b40ca1209b14f37a84a8b3
DunkinChung <55110481+DunkinChung@users.noreply.github.com>
2019-12-10 14:06:40 -0700

    added favorites page

--- includes/homefavs.inc.php
+++ includes/homefavs.inc.php
@@ -1,11 +1,13 @@
 <div>
     <p>Favourites</p>
-    <?php
-    if (isset($_SESSION['favPhotos'])) {
-        foreach ($_SESSION['favPhotos'] as $i) {
-            $img = '<img src="https://storage.googleapis.com/riley_comp3512_ass1_images/case-travel-master/images/large1024/"' . $i . ">";
-            echo $img;
+    <div id="favGrid">
+        <?php
+        if (isset($_SESSION['favPhotos'])) {
+            foreach ($_SESSION['favPhotos'] as $i) {
+                $img = '<img src="https://storage.googleapis.com/riley_comp3512_ass1_images/case-travel-master/images/small320/' . $i . '">';
+                echo $img;
+            }
         }
-    }
-    ?>
+        ?>
+    </div>
 </div>
